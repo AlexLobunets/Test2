8dd59a862c8d07085976aa1d20ad4907a54f820e
Alex Lobunets <lobuniets@gmail.com>
2014-06-11 19:02:33 +0300

    del temp comments

--- src/test2poly/Jek_Buhgalter.java
+++ src/test2poly/Jek_Buhgalter.java
@@ -1,6 +1,5 @@
 package test2poly;
 
-//import java.util.Scanner;
 
 public class Jek_Buhgalter extends VissugUsers implements EnterVissug {
 
@@ -8,23 +7,13 @@ public class Jek_Buhgalter extends VissugUsers implements EnterVissug {
 		super(name);
 	}
 	
-	//private String Pass = "87654321";
-	//Scanner scan = new Scanner(System.in);
 	
 	public boolean Enter(String name, String Pass) {
 		System.out.println(name);
-		//System.out.println("Input Password:");
-		//String s = scan.nextLine();
-		
-		//if (s.equals(this.Pass)) {
-			//System.out.println("Success!");
-			System.out.println("Buhgalter functional is available");
+		System.out.println("Buhgalter functional is available");
 			return true;
-		//}	
-		/*else {
-			System.out.println("Error! Incorrect password");
-			return false;
-		}	*/
+		
+		}
 	}
 	
 }
